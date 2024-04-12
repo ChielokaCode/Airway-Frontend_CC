4223311036f09628116d4832d3c76bc5f0b7436d
ChielokaCode <realmatec01@gmail.com>
2024-04-12 02:12:27 +0100

    initial commit

--- src/Pages/AddFlightPage.jsx
+++ src/Pages/AddFlightPage.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import AddFlightForm from "../Components/AddFlightPage/AddFlightForm.jsx";
 import AdminDashboardSide from "../Components/AdminDashboardSidebar/AdminDashboardSide.jsx";
-import AdminDashboardNavbar from "../Components/AdminDashboardNavbar/AdminDashboardNavbar.jsx";
+import AdminDashboardNavbar from "/src/Components/AdminDashboardNavbar/AdminDashboardNavbar.jsx";
 
 
 function AddFlightPage () {
     return (
         <>
             <AdminDashboardSide/>
-            <AdminDashboardNavbar />
+            <AdminDashboardNavbar/>
             <AddFlightForm />
         </>
 
